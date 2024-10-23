52bddbd07aaeac41c27659d319336fb20e4c6600
ivanono156 <74437367+ivanono156@users.noreply.github.com>
2024-10-22 18:56:29 -0700

    faster query for sorting stars

--- src/SingleMovieServlet.java
+++ src/SingleMovieServlet.java
@@ -29,115 +29,82 @@ public class SingleMovieServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Response MIME type
         response.setContentType("application/json");
-        // Retrieve parameter id from the url
         String id = request.getParameter("id");
-        // Log message can be found in localhost log
         request.getServletContext().log("getting id " + id);
-        // Output stream to STDOUT
         PrintWriter out = response.getWriter();
 
-        // Get a connection from the database
-        try (Connection conn = dataSource.getConnection()) {
-            // Construct query
+        String query = "select m.id, m.title, m.year, m.director, r.rating, " +
+                "g.id, g.name, smp.id, smp.name, smp.moviesPlayed " +
+                "from movies as m inner join stars_in_movies as sim on m.id = sim.movieId " +
 
-            String query1 = "select * " +
-                    "from (((((movies as m " +
-                    "inner join stars_in_movies as sim on m.id = sim.movieId) " +
-                    "inner join stars as s on s.id = sim.starId) " +
-                    "left join genres_in_movies as gim on gim.movieId = m.id) " +
-                    "left join genres as g on gim.genreId = g.id) " +
-                    "left join ratings as r on r.movieId = m.id) " +
-                    "where m.id = ? " +
-                    "order by g.name";
+                // Find how many movies each star played in (starMoviesPlayed as smp)
+                "inner join (select s.id, s.name, count(s.id) as moviesPlayed " +
+                // Filter out only stars that acted in this movie first
+                "from (select s.id, s.name from stars as s " +
+                "inner join stars_in_movies as sim on s.id = sim.starId " +
+                "inner join movies as m on sim.movieId = m.id " +
+                "where m.id = ?) as s " +
+                "inner join stars_in_movies as sim on s.id = sim.starId " +
+                "group by (s.id)) as smp on sim.starId = smp.id " +
 
-            String starsByMoviesPlayedQuery = "select s.id, s.name, count(s.id) as moviesPlayed " +
-                    "from stars as s inner join stars_in_movies as sim on s.id = sim.starId " +
-                    "group by (s.id) " +
-                    "order by moviesPlayed desc, s.name";
+                // Find the genres in the movie
+                "inner join genres_in_movies as gim on gim.movieId = m.id " +
+                "inner join genres as g on g.id = gim.genreId " +
 
-            String genresInMovieQuery = "select g.id, g.name, m.id " +
-                    "from genres as g inner join genres_in_movies as gim on g.id = gim.genreId " +
-                    "inner join movies as m on gim.movieId = m.id " +
-                    "where m.id = ?" +
-                    "order by g.name";
+                // Find ratings for the movie
+                "left join ratings as r on r.movieId = m.id " +
+                "where m.id = ? " +
+                "order by g.name, moviesPlayed desc, smp.name";
 
-            String query = "select m.id, m.title, m.year, m.director, r.rating, " +
-                    "g.id, g.name, smp.id, smp.name, smp.moviesPlayed " +
-                    "from movies as m inner join stars_in_movies as sim on m.id = sim.movieId " +
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setString(1, id);
+            statement.setString(2, id);
 
-                    // Find how many movies each star played in (starMoviesPlayed as smp)
-                    "inner join (select s.id, s.name, count(s.id) as moviesPlayed " +
-                    "from stars as s inner join stars_in_movies as sim on s.id = sim.starId " +
-                    "group by (s.id)) as smp on sim.starId = smp.id " +
+            try (ResultSet rs = statement.executeQuery()) {
+                JsonObject jsonObject = new JsonObject();
+                JsonObject genres = new JsonObject();
+                JsonObject stars = new JsonObject();
 
-                    // Find the genres in the movie
-                    "inner join genres_in_movies as gim on gim.movieId = m.id " +
-                    "inner join genres as g on g.id = gim.genreId " +
+                while (rs.next()) {
+                    String movieId = rs.getString("m.id");
+                    String movieTitle = rs.getString("m.title");
+                    String movieYear = rs.getString("m.year");
+                    String movieDirector = rs.getString("m.director");
+                    String movieRating = rs.getString("r.rating");
 
-                    // Find ratings for the movie
-                    "left join ratings as r on r.movieId = m.id " +
-                    "where m.id = ? " +
-                    "order by g.name, moviesPlayed desc, smp.name";
+                    String genreId = rs.getString("g.id");
+                    String genreName = rs.getString("g.name");
 
+                    String starId = rs.getString("smp.id");
+                    String starName = rs.getString("smp.name");
 
-            // Declare statement
-            try (PreparedStatement statement = conn.prepareStatement(query)) {
-                // Set the parameter to the given movie id
-                statement.setString(1, id);
+                    jsonObject.addProperty("movie_id", movieId);
+                    jsonObject.addProperty("movie_title", movieTitle);
+                    jsonObject.addProperty("movie_year", movieYear);
+                    jsonObject.addProperty("movie_director", movieDirector);
+                    jsonObject.addProperty("movie_rating", movieRating);
 
-                // Perform the query
-                try (ResultSet rs = statement.executeQuery()) {
-                    JsonObject jsonObject = new JsonObject();
-                    JsonObject genres = new JsonObject();
-                    JsonObject stars = new JsonObject();
-
-                    // Iterate through each row of the result set
-                    while (rs.next()) {
-                        // Get movie info
-                        String movieId = rs.getString("m.id");
-                        String movieTitle = rs.getString("m.title");
-                        String movieYear = rs.getString("m.year");
-                        String movieDirector = rs.getString("m.director");
-                        String movieRating = rs.getString("r.rating");
-                        // Get genre info
-                        String genreId = rs.getString("g.id");
-                        String genreName = rs.getString("g.name");
-                        // Get star info
-                        String starId = rs.getString("smp.id");
-                        String starName = rs.getString("smp.name");
-
-                        jsonObject.addProperty("movie_id", movieId);
-                        jsonObject.addProperty("movie_title", movieTitle);
-                        jsonObject.addProperty("movie_year", movieYear);
-                        jsonObject.addProperty("movie_director", movieDirector);
-                        jsonObject.addProperty("movie_rating", movieRating);
-                        // Add genres to json object
-                        genres.addProperty(genreId, genreName);
-                        // Add stars to json object
-                        stars.addProperty(starId, starName);
-                    }
-
-                    jsonObject.add("movie_genres", genres);
-                    jsonObject.add("movie_stars", stars);
-
-                    // Write JSON string to output
-                    out.write(jsonObject.toString());
-                    // Set response status to 200 (OK)
-                    response.setStatus(200);
+                    genres.addProperty(genreId, genreName);
+                    stars.addProperty(starId, starName);
                 }
+
+                jsonObject.add("movie_genres", genres);
+                jsonObject.add("movie_stars", stars);
+
+                out.write(jsonObject.toString());
+                // Set response status to 200 (OK)
+                response.setStatus(HttpServletResponse.SC_OK);
             }
         } catch (Exception e) {
-            // Write error message JSON object to output
             JsonObject jsonObject = new JsonObject();
             jsonObject.addProperty("errorMessage", e.getMessage());
             out.write(jsonObject.toString());
 
-            // Log error to localhost log
             request.getServletContext().log("Error:", e);
             // Set response status to 500 (Internal Server Error)
-            response.setStatus(500);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         } finally {
             out.close();
         }
