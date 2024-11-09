57a2034f507c1e5623b4a4f0ef2dd7ae6c6a9b44
ariasc3 <ariasc3@uci.edu>
2024-11-08 20:01:37 -0800

    added employee pages

---
+++ WebContent/employee_login.js
@@ -0,0 +1,37 @@
+let Employee_login_form = $("#employee-login-form");
+
+function handleEmployeeLoginResult(resultData) {
+
+    console.log("handle login response");
+    console.log(resultData);
+    console.log(resultData["status"]);
+
+    // If login succeeds, it will redirect the user to index.html
+    if (resultData["status"] === "success") {
+        window.location.replace("index.html");
+    } else {
+        // If login fails, the web page will display
+        // error messages on <div> with id "login_error_message"
+        console.log("show error message");
+        console.log(resultData["message"]);
+        $("#employee_login_error_message").text(resultData["message"]);
+    }
+}
+
+function submitEmployeeLoginForm(formSubmitEvent) {
+    console.log("submit login form");
+
+    formSubmitEvent.preventDefault();
+
+    $.ajax(
+        "api/employee-login", {
+            method: "POST",
+            // Serialize the login form to the data sent by POST request
+            data: Employee_login_form.serialize(),
+            success: handleEmployeeLoginResult
+        });
+}
+
+Employee_login_form.submit(submitEmployeeLoginForm);
+
+
---
+++ src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+
+    private String email;
+
+    public Employee(String email) {
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+}
