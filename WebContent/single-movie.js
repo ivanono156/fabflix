fce8390dc2b5b9cd1d9544781ebd7aab769aa217
ivanono156 <74437367+ivanono156@users.noreply.github.com>
2024-11-13 23:15:40 -0800

    order genres and stars properly in js file

--- WebContent/single-movie.js
+++ WebContent/single-movie.js
@@ -1,22 +1,4 @@
-/**
- * This example is following frontend and backend separation.
- *
- * Before this .js is loaded, the html skeleton is created.
- *
- * This .js performs three steps:
- *      1. Get parameter from request URL so it knows which id to look for
- *      2. Use jQuery to talk to backend API to get the json data.
- *      3. Populate the data to correct html elements.
- */
-
-
-/*
- * Retrieve parameter from request URL, matching by parameter name
- * @param target String
- * @returns {*}
- */
 function getParameterByName(target) {
-    // Get request URL
     let url = window.location.href;
     // Encode target parameter name to url encoding
     target = target.replace(/[\[\]]/g, "\\$&");
@@ -31,73 +13,57 @@ function getParameterByName(target) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-/**
- * Handles the data returned by the API, read the jsonObject and populate data into html elements
- * @param resultData jsonObject
- */
-
 function handleResult(resultData) {
 
-    // Setting webpage title
     document.title = resultData["movie_title"];
 
     console.log("handleResult: populating movie info from resultData");
+    console.log(resultData);
 
-    // find the empty h1 body by id "movie_title"
     let movieTitleElement = jQuery("#movie_title");
 
-    // add the movie title to the h1 element
     movieTitleElement.append("<p>" + resultData["movie_title"] + "</p>");
 
-    // find the empty h3 body by id "movie_info"
     let movieInfoElement = jQuery("#movie_info");
 
-    // append two html <p> created to the h3 body, which will refresh the page
     movieInfoElement.append("<p>Release Year: " + resultData["movie_year"] + "</p>" +
         "<p>Director: " + resultData["movie_director"] + "</p>" +
         "<p>Rating: " + resultData["movie_rating"] + "</p>");
 
     console.log("handleResult: populating genre table from resultData");
 
-    // Find the empty table body by id "genre_table_body"
     let genreTableBodyElement = jQuery("#genre_table_body");
 
-    // Json array containing this movie's genres
     const genres = resultData["movie_genres"];
 
-    // Concatenate the html tags with resultData jsonObject to create table rows
-    for (const genreId in genres) {
+    for (let i = 0; i < genres.length; i++) {
         let rowHTML = "";
         rowHTML += "<tr>";
         rowHTML +=
             "<td>"
-            // Add a link to browse by genre
-            + '<a href="movie-list.html?' + createDefaultMovieListUrl() + '&' + createUrlParams(genreKeyName, genreId) + '">'
-            + genres[genreId] // Display genre name
+            + '<a href="movie-list.html?' + createDefaultMovieListUrl()
+            + '&' + createUrlParams(genreKeyName, genres[i]["genre_id"]) + '">'
+            + genres[i]["genre_name"]
             + '</a>'
             + "</td>";
         rowHTML += "</tr>";
 
-        // Append the row created to the table body, which will refresh the page
         genreTableBodyElement.append(rowHTML);
     }
 
     console.log("handleResult: populating star table from resultData");
 
-    // Find the empty table body by id "star_table_body"
     let starTableBodyElement = jQuery("#star_table_body");
 
-    // Json array containing movies this star acted in
     const stars = resultData["movie_stars"];
 
-    // Concatenate the html tags with resultData jsonObject to create table rows
-    for (const starId in stars) {
+    for (let i = 0; i < stars.length; i++) {
         let rowHTML = "";
         rowHTML += "<tr>";
-        rowHTML += "<td>" + '<a href="single-star.html?id=' + starId + '">' + stars[starId] + '</a>' + "</td>";
+        rowHTML += "<td>" + '<a href="single-star.html?id=' + stars[i]["star_id"] + '">'
+            + stars[i]["star_name"] + '</a>' + "</td>";
         rowHTML += "</tr>";
 
-        // Append the row created to the table body, which will refresh the page
         starTableBodyElement.append(rowHTML);
     }
 
@@ -114,17 +80,11 @@ function handleResult(resultData) {
     backToMovieListElement.append('<a href="' + href + '" class="back-btn">Back to Movies List</a>');
 }
 
-/**
- * Once this .js is loaded, following scripts will be executed by the browser
- */
-
-// Get id from URL
 let movieId = getParameterByName('id');
 
-// Makes the HTTP GET request and registers on success callback function handleResult
 jQuery.ajax({
-    dataType: "json",  // Setting return data type
-    method: "GET",// Setting request method
-    url: "api/single-movie?id=" + movieId, // Setting request url, which is mapped by StarsServlet in Stars.java
-    success: (resultData) => handleResult(resultData) // Setting callback function to handle data returned successfully by the SingleStarServlet
+    dataType: "json",
+    method: "GET",
+    url: "api/single-movie?id=" + movieId,
+    success: (resultData) => handleResult(resultData)
 });
--- src/SingleMovieServlet.java
+++ src/SingleMovieServlet.java
@@ -1,3 +1,4 @@
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import jakarta.servlet.ServletConfig;
@@ -10,7 +11,6 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -20,27 +20,27 @@ public class SingleMovieServlet extends HttpServlet {
     private static final long serialVersionUID = 2L;
 
     private static final String singleMovieQuery = "select m.id, m.title, m.year, m.director, r.rating, " +
-                "g.id, g.name, smp.id, smp.name, smp.moviesPlayed " +
-                "from movies as m inner join stars_in_movies as sim on m.id = sim.movieId " +
+            "g.id, g.name, smp.id, smp.name, smp.moviesPlayed " +
+            "from movies as m inner join stars_in_movies as sim on m.id = sim.movieId " +
 
-                // Find how many movies each star played in (starMoviesPlayed as smp)
-                "inner join (select s.id, s.name, count(s.id) as moviesPlayed " +
-                // Filter out only stars that acted in this movie first
-                "from (select s.id, s.name from stars as s " +
-                "inner join stars_in_movies as sim on s.id = sim.starId " +
-                "inner join movies as m on sim.movieId = m.id " +
-                "where m.id = ?) as s " +
-                "inner join stars_in_movies as sim on s.id = sim.starId " +
-                "group by (s.id)) as smp on sim.starId = smp.id " +
+            // Find how many movies each star played in (starMoviesPlayed as smp)
+            "inner join (select s.id, s.name, count(s.id) as moviesPlayed " +
+            // Filter out only stars that acted in this movie first
+            "from (select s.id, s.name from stars as s " +
+            "inner join stars_in_movies as sim on s.id = sim.starId " +
+            "inner join movies as m on sim.movieId = m.id " +
+            "where m.id = ?) as s " +
+            "inner join stars_in_movies as sim on s.id = sim.starId " +
+            "group by (s.id)) as smp on sim.starId = smp.id " +
 
-                // Find the genres in the movie
-                "inner join genres_in_movies as gim on gim.movieId = m.id " +
-                "inner join genres as g on g.id = gim.genreId " +
+            // Find the genres in the movie
+            "inner join genres_in_movies as gim on gim.movieId = m.id " +
+            "inner join genres as g on g.id = gim.genreId " +
 
-                // Find ratings for the movie
-                "left join ratings as r on r.movieId = m.id " +
-                "where m.id = ? " +
-                "order by g.name, moviesPlayed desc, smp.name";
+            // Find ratings for the movie
+            "left join ratings as r on r.movieId = m.id " +
+            "where m.id = ? " +
+            "order by g.name, moviesPlayed desc, smp.name";
 
     private DataSource dataSource;
 
@@ -66,8 +66,8 @@ public class SingleMovieServlet extends HttpServlet {
 
             try (ResultSet rs = statement.executeQuery()) {
                 JsonObject jsonObject = new JsonObject();
-                JsonObject genres = new JsonObject();
-                JsonObject stars = new JsonObject();
+                JsonArray genresJsonArray = new JsonArray();
+                JsonArray starsJsonArray = new JsonArray();
 
                 while (rs.next()) {
                     String movieId = rs.getString("m.id");
@@ -91,12 +91,23 @@ public class SingleMovieServlet extends HttpServlet {
                     jsonObject.addProperty("movie_director", movieDirector);
                     jsonObject.addProperty("movie_rating", movieRating);
 
-                    genres.addProperty(genreId, genreName);
-                    stars.addProperty(starId, starName);
+                    JsonObject genreJsonObject = new JsonObject();
+                    genreJsonObject.addProperty("genre_id", genreId);
+                    genreJsonObject.addProperty("genre_name", genreName);
+                    if (!genresJsonArray.contains(genreJsonObject)) {
+                        genresJsonArray.add(genreJsonObject);
+                    }
+
+                    JsonObject starJsonObject = new JsonObject();
+                    starJsonObject.addProperty("star_id", starId);
+                    starJsonObject.addProperty("star_name", starName);
+                    if (!starsJsonArray.contains(starJsonObject)) {
+                        starsJsonArray.add(starJsonObject);
+                    }
                 }
 
-                jsonObject.add("movie_genres", genres);
-                jsonObject.add("movie_stars", stars);
+                jsonObject.add("movie_genres", genresJsonArray);
+                jsonObject.add("movie_stars", starsJsonArray);
 
                 response.getWriter().write(jsonObject.toString());
                 response.setStatus(HttpServletResponse.SC_OK);
